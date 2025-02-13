4c78e6343f00e2b7602c8a66b0145424a78c3b12
Betty M <bttyty@gmail.com>
2025-02-13 00:37:05 -0500

    Updated swagger api-doc more

--- app/api/items/listings/route.ts
+++ app/api/items/listings/route.ts
@@ -22,6 +22,6 @@ export async function GET(request: NextRequest) {
         return NextResponse.json({ message: "User not found! Please sign in!" }, { status: 404 });
     }
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
--- app/lib/swagger.ts
+++ app/lib/swagger.ts
@@ -278,21 +278,21 @@ export const getApiDocs = async () => {
             },
           },
         },
-        '/profile':{
+        "/profile": {
           put: {
-            tags: ['Users'],
-            description: 'Update the user\'s profile',
-            summary: 'update user\'s profile',
-          }
+            tags: ["Users"],
+            description: "Update the user's profile",
+            summary: "Update user's profile",
+          },
         },
-        '/items/item':{
+        "/items/item": {
           get: {
-            tags:['Items'],
-            description: 'Get all items sorted',
-            summary: 'Get all items',
-            responses:{
+            tags: ["Items"],
+            description: "Get all items sorted",
+            summary: "Get all items",
+            responses: {
               200: {
-                description: 'Items found',
+                description: "Items found",
                 content: {
                   "application/json": {
                     schema: {
@@ -310,73 +310,235 @@ export const getApiDocs = async () => {
               400: {
                 description: "There are no items.",
               },
-            }
-          }
+            },
+          },
         },
-        '/items/[id]':{
-          patch:{
-            tags:['Items'],
-            description: 'Patch/edit item by Id',
-            summary: 'edit item',
-            requestBody:{
-              content:{
-                'application/json':{
-                  schema:{
-                    type: 'object',
+        "/items/[id]": {
+          patch: {
+            tags: ["Items"],
+            description: "Patch/edit item by Id",
+            summary: "Edit item's title, description,price or status",
+            requestBody: {
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
                     properties: {
-                      name:{
-                        type:'string',
-                        example: 'Test Item',
+                      name: {
+                        type: "string",
+                        example: "Test Item",
                       },
                       description: {
-                        type: 'string',
-                        example: 'This is a test item'
+                        type: "string",
+                        example: "This is a test item",
                       },
                       price: {
-                        type: 'float',
+                        type: "float",
                         example: 4.99,
                       },
-                      status:{
-                        type:'string',
-                        example: 'available'
-                      }
-                    }
-                  }
-                }
-              }
+                      status: {
+                        type: "string",
+                        example: "available",
+                      },
+                    },
+                  },
+                },
+              },
             },
             responses: {
               200: {
-                description: 'Item updated successfully.',
+                description: "Item updated successfully.",
                 content: {
-                  'application/json':{
-                    schema:{
-                      type: 'object',
+                  "application/json": {
+                    schema: {
+                      type: "object",
                       properties: {
-                        name:{
-                          type:'string',
-                          example: 'Test Item',
+                        name: {
+                          type: "string",
+                          example: "Test Item",
                         },
                         description: {
-                          type: 'string',
-                          example: 'This is a test item'
+                          type: "string",
+                          example: "This is a test item",
                         },
                         price: {
-                          type: 'float',
+                          type: "float",
                           example: 3.99,
                         },
-                        status:{
-                          type:'string',
-                          example: 'sold'
-                        }
-                      }
-                    }
-                  }
-                }
+                        status: {
+                          type: "string",
+                          example: "sold",
+                        },
+                      },
+                    },
+                  },
+                },
               },
-              
+              500: {
+                description: "Error updating item!",
+              },
             },
-          }
+          },
+        },
+        "/items/createlisting": {
+          post: {
+            tags: ["Items"],
+            description: "Create a listing",
+            summary: "Create a listing",
+            requestBody: {
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      title: {
+                        type: "string",
+                        example: "old TV",
+                      },
+                      description: {
+                        type: "string",
+                        example: "This is a test item",
+                      },
+                      price: {
+                        type: "float",
+                        example: 4.99,
+                      },
+                      location: {
+                        type: "string",
+                        example: "Maryland",
+                      },
+                      category: {
+                        type: "string",
+                        example: "Electronics",
+                      },
+                      owner: {
+                        type: "string",
+                        example: "67abe8bc98863972d8b944df",
+                      },
+                      image: {
+                        type: "string",
+                        example: "tv.png",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            responses: {
+              201: {
+                description: "Item created successfully.",
+                content: {
+                  "application/json": {
+                    schema: {
+                      type: "object",
+                      properties: {
+                        title: {
+                          type: "string",
+                          example: "old TV",
+                        },
+                        description: {
+                          type: "string",
+                          example: "This is a test item",
+                        },
+                        price: {
+                          type: "float",
+                          example: 4.99,
+                        },
+                        location: {
+                          type: "string",
+                          example: "Maryland",
+                        },
+                        category: {
+                          type: "string",
+                          example: "Electronics",
+                        },
+                        owner: {
+                          type: "string",
+                          example: "67abe8bc98863972d8b944df",
+                        },
+                        image: {
+                          type: "string",
+                          example: "1737089296412tv.png",
+                        },
+                        createdAt: {
+                          type: "date",
+                          example: "2025-02-11T14:15:52.555+00:00",
+                        },
+                        updatedAt: {
+                          type: "date",
+                          example: "2025-02-11T14:15:52.555+00:00",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              500: {
+                description: "Internal server error.",
+              },
+            },
+          },
+        },
+        "/items/deleteitem": {
+          delete: {
+            tags: ["Items"],
+            description: "Delete selected item.",
+            summary: "Delete an item by id",
+            requestBody: {
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      owner: {
+                        type: "string",
+                        example: "67abe8bc98863972d8b944df",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+            responses: {
+              200: {
+                description: "Item deleted!",
+                content: {
+                  "aplication/json": {
+                    schema: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example: "Item deleted successfully!",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              500: {
+                description: "Internal server error",
+              },
+            },
+          },
+        },
+        "/items/listing": {
+          get: {
+            tags: ["Items"],
+            description: "List all items posted by a user",
+            summary: "Get all items by user",
+            responses: {
+              200: {
+                description: "Fetch items posted by user",
+              },
+              404: {
+                Description: "No items for user",
+              },
+              500: {
+                description: "Internal server error",
+              },
+            },
+          },
         },
         security: [],
       },
